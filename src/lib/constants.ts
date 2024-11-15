4d6fd94320b4f08abc5ee18aee47760bcffb11c6
Josh Grewal <veerjsg@outlook.com>
2024-11-15 17:52:08 -0500

    Initial

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,12 +7,10 @@ export const loaderAnimation = [
 ]
 
 export const LINKS = {
-  github: 'https://github.com/bue221',
-  linkedin: 'https://www.linkedin.com/in/bue221/',
-  mail: 'mailto:camiloplaza3@gmail.com',
-  instagram: 'https://www.instagram.com/caj_ink/',
-  medium: 'https://ladvace.medium.com/',
-  discord: 'https://discordapp.com/users/163300027618295808',
+  github: 'https://github.com/Joshveer',
+  linkedin: 'https://www.linkedin.com/in/joshveer-grewal/',
+  mail: 'mailto:joshveergrewal@gmail.com',
+  instagram: 'https://www.instagram.com/joshveergrewal/',
 }
 
 // Global
@@ -50,111 +48,54 @@ export const SEARCH: Page = {
 // Study Page
 export const STUDIES = [
   {
-    title: 'Computer Science',
-    institution: 'Universidad Central',
-    link: 'https://www.ucentral.edu.co/',
-    date: '2019 - 2027',
+    title: 'High School Student',
+    institution: 'Northville High School',
   },
   {
-    title: '...',
-    institution: 'Platzi',
-    link: 'https://platzi.com/',
-    date: '2018 - 2022',
+    title: 'Dual Enrolled Student',
+    institution: 'Schoolcraft College',
   },
   {
-    title: '...',
-    institution: 'Udemy',
-    link: 'https://www.udemy.com/',
-    date: '2018 - 2022',
-  },
-  {
-    title: '...',
-    institution: 'Youtube',
-    link: 'https://www.youtube.com/@midulive',
-    date: '2018 - 2022',
-  },
-  {
-    title: 'Tattoo Artist',
-    institution: 'Tattoo Academy',
-    link: 'https://www.instagram.com/tattoodcacademy/',
-    date: '2024 - 2025',
-  },
+    title: 'Dual Enrolled Student',
+    institution: 'Wasthenaw Community College',
+  }
 ]
 
 export const EXPERIENCE = [
   {
-    company: 'Straico',
-    location: 'Bogotá D C, Colombia',
-    position: 'Software Engineer',
-    start: '2021',
-    link: 'https://straico.com/',
-    end: 'Current',
+    company: 'Kohandel\'s Lab @ University of Waterloo',
+    location: 'Waterloo, Ontario, Canada - Virtual',
+    position: 'Programmer, Researcher, Co-Author',
+    start: 'Feb 2024',
+    link: 'https://uwaterloo.ca/kohandel-lab/',
+    end: 'Nov 2024',
     tasks: [
-      'Integration IA services with react and tailwind css',
-      'Development and build of DB with mongoDB',
+      'Expected arXiv and SISC publication in Nov 2024. ',
+      'I applied Buckingham π Theorem & SymbolicGPT to reduce time & data needed to find unknown functions in ODEs/PDEs.',
     ],
   },
   {
-    company: 'Spot2',
-    location: 'Mexico City, Mexico',
-    position: 'Software Engineer',
-    link: 'https://spot2.mx/',
-    start: '2021',
-    end: 'Current',
+    company: 'H.O.P.E. Lab @ University of Michigan',
+    location: 'Ann Arbor, Michigan, USA - Hybrid',
+    position: 'Student Fellow',
+    link: 'https://media.dent.umich.edu/labs/hope/',
+    start: 'Jul 2024',
+    end: 'Aug 2024',
     tasks: [
-      'Development of the Spot2 platform with the use of React, Redux, and Material UI',
-      'Development map with the use of Google Maps API',
-      'Development internal platform with the use of React, Redux, and Material UI',
-      'Testing and debugging',
+      '1 of 3 applicants selected. ',
+      'Worked under Dr. DaSilva on noninvasive craniofacial pain treatments using tDCS, fMRI, & fNIRS. Wrote a literature review on Mathematical Oncology (available on LinkedIn).',
     ],
   },
   {
-    company: 'Imaginamos',
-    link: 'https://imaginamos.com/',
-    location: 'Bogotá D C, Colombia',
-    position: 'Frontend developer',
-    start: '2021',
-    end: '2021',
+    company: 'Harvard University & MIT',
+    link: 'https://arxiv.org/abs/2407.02673',
+    location: 'Massachusetts, United States - Remote',
+    position: 'Co-Researcher & Co-Author',
+    start: 'Jul 2023',
+    end: 'Jul 2024',
     tasks: [
-      'Development of the Imaginamos platform with the use of React, Redux, and Material UI',
-      'Work in ETB project with the use of React, Redux, and Material UI',
+      'arXiv publication on July 2nd, 2024 (linked).',
+      'Worked in a team of 3 alongside Dr. Ponce (Harvard) & Dr. Arslan (MIT) to expand upon DreamDiffusion technology by connecting latent spaces with StableDiffusion & updating OOS codebase on Google Colab.',
     ],
-  },
-  {
-    company: 'INETUM',
-    location: 'Bogotá D C, Colombia',
-    position: 'Frontend developer',
-    start: '2021',
-    link: 'https://www.inetum.com/es',
-    end: '2021',
-    tasks: [
-      'Support in the QA area and bug review',
-      'Use of SCRUM methodology',
-      'Claro projects with the use of frameworks and libraries such as react and angular',
-      'Use of redux toolkit as aproposal which allowed a shorter development time when using this tool',
-    ],
-  },
-  {
-    company: 'S I G',
-    location: 'Bogotá D C, Colombia',
-    position: 'Software developer',
-    start: '2021',
-    end: '2021',
-    tasks: [
-      'Development of a dashboard with react admin and chartjs for managing and entering information',
-    ],
-  },
-  {
-    company: 'BOOKII',
-    location: 'Bogotá D C, Colombia',
-    position: 'Software developer',
-    start: '2019',
-    end: '2021',
-    tasks: [
-      'Productive collaborationwiththebackendteamforthecreationofthe conversationalclubs',
-      'Effective coding of conversational clubs following design guide lines and using the Redux statemanager',
-      'Creation of the Bookii page in Spanish and change of texts in cms',
-      'Generation of static posts using Gatsby and the Contentful cms for the Bookii blog',
-    ],
-  },
+  }
 ]
